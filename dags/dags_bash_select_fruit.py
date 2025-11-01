db3935cd79742f31baa3e95c800bf098db519281
v <v>
2025-11-01 14:23:57 +0900

    dags: fix dags_bash_select_fruit.py

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -7,17 +7,17 @@ with DAG(
     dag_id="dags_bash_select_fruit",
     schedule="10 0 * * 6#1",
     start_date = pendulum.datetime(2025, 11, 1, tz="Asia/Seoul"),
-    tags=["inflearn"], 
+    tags=["inflearn"],
     catchup=False,
 ) as dag:
     t1_orange = BashOperator(
         task_id="t1_orange",
-        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE"
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE",
     )
 
     t2_avocado = BashOperator(
         task_id="t2_avocado",
-        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO"
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
     )
 
     t1_orange >> t2_avocado
