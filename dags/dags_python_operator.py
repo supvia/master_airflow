b251d41213038fe04a1aff82514187ac7f096825
v <v>
2025-11-01 16:53:17 +0900

    dags: add dags_python_operator.py

---
+++ dags/dags_python_operator.py
@@ -0,0 +1,24 @@
+import pendulum
+import random
+
+
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import DAG
+
+with DAG(
+    dag_id="dags_python_operator",
+    schedule=None,
+    tags=["inflearn"],
+    catchup=False,
+) as dags:
+    def select_fruit():
+        fruit = ["APPLE", "BANANA", "ORANGE", "AVOCADO"]
+        rand_int = random.randint(0, len(fruit) - 1)
+        print(f"Selected fruit: {fruit[rand_int]}")
+    
+    py_t1 = PythonOperator(
+        task_id="py_t1",
+        python_callable=select_fruit
+    )
+    
+    py_t1
